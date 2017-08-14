fda12d28abfcaf2186c0023a89090b2ba5c80de4
nitingupta220 <nitin16@navgurukul.org>
2017-08-14 14:41:40 +0530

    add the files

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(angular) {
+
+    var app = angular.module("json", []);
+
+    app.controller("json_work", ["$scope", "$http", function($scope, $http) {
+        //    here $http get a information from "logs_with_gates.json"     
+
+        $http.get("logs_with_gates.json")
+            //        then we will call then mathod to call function in which response argument gives all data contain in records as an Array 
+            .then(function(response) {
+
+                $scope.mydata = response.data;
+
+                console.log(response.data[0].gate_list);
+
+            });
+        $scope.list1 = [1, 2, 3, 4];
+    }]);
+})(window.angular)
